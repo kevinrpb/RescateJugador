428b4c8f61f5565444d65605a53baed31e43efba
GodNietzsche <36699393+GodNietzsche@users.noreply.github.com>
2019-11-21 16:37:26 +0100

    Creacion de planes y beliefs agente-tablero.xml

---
+++ src/rescate/tablero/planes/ExtinguirHumoPlan.java
@@ -0,0 +1,18 @@
+package rescate.tablero.planes;
+
+import java.util.*;
+
+import jadex.adapter.fipa.*;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+
+import rescate.ontologia.*;
+
+class ExtinguirHumoPlan extends Plan {
+
+	@Override
+	public void body() {
+
+	}
+
+}
