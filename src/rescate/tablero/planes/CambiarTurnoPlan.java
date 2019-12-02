bd3ca22cacc81ff36d2c84e737967c1c83fc825c
vgranado <100348903@alumnos.uc3m.es>
2019-12-02 13:59:06 +0100

    CambiarTurno plan

---
+++ src/rescate/tablero/planes/CambiarTurnoPlan.java
@@ -0,0 +1,18 @@
+package rescate.tablero.planes;
+
+import jadex.adapter.fipa.*;
+import jadex.runtime.IMessageEvent;
+import jadex.runtime.Plan;
+
+import rescate.ontologia.acciones.*;
+import rescate.ontologia.conceptos.*;
+import rescate.ontologia.predicados.*;
+
+class AbrirPuertaPlan extends Plan {
+
+	@Override
+	public void body() {
+    
+	}
+
+}
