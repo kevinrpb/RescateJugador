c8fe49f0516f41cd23b5a48212d45ed746255793
Slipkvoc <100348736@alumnos.uc3m.es>
2020-01-06 10:25:28 +0100

    Añadida comprobación al acabar partida para que no termine la partida varias veces

--- src/rescate/tablero/planes/AcabarPartidaPlan.java
+++ src/rescate/tablero/planes/AcabarPartidaPlan.java
@@ -14,30 +14,32 @@ public class AcabarPartidaPlan extends Plan {
 	@Override
 	public void body() {
 
-    System.out.println("[PLAN] El tablero acaba la partida");
-
-    // Tablero y jugadores
-    Tablero t = (Tablero) getBeliefbase().getBelief("tablero").getFact();
-    ArrayList<Jugador> jugadores = t.getJugadores();
-
-    // Beliefs y resultado
-    int salvados = (int) getBeliefbase().getBelief("salvados").getFact();
-
-    // Se informa a todos los jugadores de que la partida ha acabado y del resultado
-    IMessageEvent respuesta = createMessageEvent("Inform_Partida_Acabada");
-    ResultadoPartida predicado = new ResultadoPartida();
-    predicado.setResultado((salvados > 6) ? 0 : 1);
-    respuesta.setContent(predicado);
-
-    // Como destinatarios, todos los jugadores
-    for (Jugador j: jugadores) {
-      respuesta.getParameterSet(SFipa.RECEIVERS).addValue(j.getIdAgente());
+    if((boolean) getBeliefbase().getBelief("jugando").getFact()){
+      System.out.println("[PLAN] El tablero acaba la partida");
+  
+      // Tablero y jugadores
+      Tablero t = (Tablero) getBeliefbase().getBelief("tablero").getFact();
+      ArrayList<Jugador> jugadores = t.getJugadores();
+  
+      // Beliefs y resultado
+      int salvados = (int) getBeliefbase().getBelief("salvados").getFact();
+  
+      // Se informa a todos los jugadores de que la partida ha acabado y del resultado
+      IMessageEvent respuesta = createMessageEvent("Inform_Partida_Acabada");
+      ResultadoPartida predicado = new ResultadoPartida();
+      predicado.setResultado((salvados > 6) ? 0 : 1);
+      respuesta.setContent(predicado);
+  
+      // Como destinatarios, todos los jugadores
+      for (Jugador j: jugadores) {
+        respuesta.getParameterSet(SFipa.RECEIVERS).addValue(j.getIdAgente());
+      }
+  
+      // Se envia la respuesta
+      sendMessage(respuesta);
+      // Se deja de jugar
+      getBeliefbase().getBelief("jugando").setFact(false);
     }
-
-    // Se envia la respuesta
-    sendMessage(respuesta);
-    // Se deja de jugar
-    getBeliefbase().getBelief("jugando").setFact(false);
   }
 
 }
