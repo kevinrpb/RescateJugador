08fad10e6d82d8853e3245f9ffc67084fc216eb4
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2020-01-13 09:40:37 +0100

    Manda jugador actualizado al elegir rol

--- src/rescate/ontologia/predicados/RolElegido.java
+++ src/rescate/ontologia/predicados/RolElegido.java
@@ -1,8 +1,20 @@
 package rescate.ontologia.predicados;
 
+import rescate.ontologia.conceptos.Jugador;
+
 public class RolElegido extends Predicado {
 
+  Jugador jugador;
+
   public RolElegido() {
+  }
+
+  public void setJugador(Jugador jugador) {
+    this.jugador = jugador;
+  }
+
+  public Jugador getJugador() {
+    return jugador;
   }
 
 }
--- src/rescate/tablero/planes/AsignarRolPlan.java
+++ src/rescate/tablero/planes/AsignarRolPlan.java
@@ -57,8 +57,34 @@ public class AsignarRolPlan extends Plan {
         getBeliefbase().getBelief("view").setFact(viewUpdater);
         // Se actualiza en la base de creencias el hecho tablero
         getBeliefbase().getBelief("tablero").setFact(t);
+
+        // Se actualizan los PA del jugador
+        jugador.setPuntosAccion(4);
+        jugador.setPuntosAccionMando(0);
+        jugador.setPuntosAccionExtincion(0);
+        jugador.setPuntosAccionMovimiento(0);
+
+        switch (accion.getRol()) {
+          case 2: // Jefe
+            jugador.setPuntosAccionMando(2);
+            break;
+          case 4: // Espuma
+            jugador.setPuntosAccion(3);
+            jugador.setPuntosAccionExtincion(3);
+            break;
+          case 6: // Generalista
+            jugador.setPuntosAccion(5);
+            break;
+          case 7: // Rescates
+            jugador.setPuntosAccionMovimiento(3);
+            break;
+        }
+
         // Se confirma al jugador la asignación del rol elegido
-        IMessageEvent respuesta = peticion.createReply("Inform_Rol_Elegido", new RolElegido());
+        RolElegido predicado = new RolElegido();
+        predicado.setJugador(jugador);
+
+        IMessageEvent respuesta = peticion.createReply("Inform_Rol_Elegido", predicado);
         sendMessage(respuesta);
       }
       // Si no...
