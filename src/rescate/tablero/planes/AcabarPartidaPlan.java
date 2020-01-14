a6aa86f89decdeb73d03d9379a8fa41b628079d7
vgranados <100348903@alumnos.uc3m.es>
2020-01-14 23:11:00 +0100

    Arreglos colPDI y finPartida

--- src/rescate/tablero/planes/AcabarPartidaPlan.java
+++ src/rescate/tablero/planes/AcabarPartidaPlan.java
@@ -3,7 +3,9 @@ package rescate.tablero.planes;
 import java.util.ArrayList;
 
 import jadex.adapter.fipa.*;
+import jadex.runtime.IGoal;
 import jadex.runtime.IMessageEvent;
+import jadex.runtime.IPlan;
 import jadex.runtime.Plan;
 
 import rescate.ontologia.conceptos.*;
@@ -11,35 +13,37 @@ import rescate.ontologia.predicados.*;
 
 public class AcabarPartidaPlan extends Plan {
 
+
 	@Override
 	public void body() {
 
-    if((boolean) getBeliefbase().getBelief("jugando").getFact()){
-      System.out.println("[PLAN] El tablero acaba la partida");
-  
-      // Tablero y jugadores
-      Tablero t = (Tablero) getBeliefbase().getBelief("tablero").getFact();
-      ArrayList<Jugador> jugadores = t.getJugadores();
-  
-      // Beliefs y resultado
-      int salvados = (int) getBeliefbase().getBelief("salvados").getFact();
-  
-      // Se informa a todos los jugadores de que la partida ha acabado y del resultado
-      IMessageEvent respuesta = createMessageEvent("Inform_Partida_Acabada");
-      ResultadoPartida predicado = new ResultadoPartida();
-      predicado.setResultado((salvados > 6) ? 0 : 1);
-      respuesta.setContent(predicado);
-  
-      // Como destinatarios, todos los jugadores
-      for (Jugador j: jugadores) {
-        respuesta.getParameterSet(SFipa.RECEIVERS).addValue(j.getIdAgente());
-      }
-  
-      // Se envia la respuesta
-      sendMessage(respuesta);
-      // Se deja de jugar
-      getBeliefbase().getBelief("jugando").setFact(false);
+    int cubosDanno = (int) getBeliefbase().getBelief("cubosDanno").getFact();
+    int victimas = (int) getBeliefbase().getBelief("victimas").getFact();
+    int salvados = (int) getBeliefbase().getBelief("salvados").getFact();
+
+    System.out.println("[PLAN] El tablero acaba la partida porque" + ((cubosDanno < 1) ? " se ha derruido la casa" : ((victimas > 3) ? " hay mas de 4 victimas" : " han salvado 7 posibles victimas")));
+
+    // Tablero y jugadores
+    Tablero t = (Tablero) getBeliefbase().getBelief("tablero").getFact();
+    ArrayList<Jugador> jugadores = t.getJugadores();
+
+    // Se informa a todos los jugadores de que la partida ha acabado y del resultado
+    IMessageEvent respuesta = createMessageEvent("Inform_Partida_Acabada");
+    ResultadoPartida predicado = new ResultadoPartida();
+    predicado.setResultado((salvados > 6) ? 0 : 1);
+    respuesta.setContent(predicado);
+
+    // Como destinatarios, todos los jugadores
+    for (Jugador j: jugadores) {
+      respuesta.getParameterSet(SFipa.RECEIVERS).addValue(j.getIdAgente());
     }
+
+    // Se envia la respuesta
+    sendMessage(respuesta);
+      
+    // Se deja de jugar
+    getBeliefbase().getBelief("jugando").setFact(false);
+
   }
 
 }
